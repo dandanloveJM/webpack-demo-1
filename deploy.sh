febcdeecc88225f9842c9380a44921f10c1a8922
dandanloveJM <gigi834921748@qq.com>
2020-07-02 13:51:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 yarn build &&
-git checkout gh-pages &&
+git checkout gh-pages2 &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
 rm -rf dist;
 git add . &&
 git commit -m 'update' &&
-git push
+git push --set-upstream origin gh-pages2
